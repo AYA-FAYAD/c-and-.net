0c955071cfa076602ecf292ea5f2c001542d86ce
aya fayad <ml.aya.fayad@gmail.com>
2025-05-04 20:32:54 +0300

    inher

---
+++ inherited/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class BaseEmployee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public virtual string GetEmployeeDetail()
+    {
+        return $"{FirstName} {LastName}";
+    }
+}
+
+public class Houremployee : BaseEmployee
+{
+    public decimal HourlRate { get; set; }
+
+    public decimal calculateWeeklyPay(int hourWorked)
+    {
+        return HourlRate * hourWorked;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        BaseEmployee employee = new BaseEmployee();
+        employee.FirstName = "aya";
+        employee.LastName = "fayad";
+        employee.GetEmployeeDetail();
+        Console.WriteLine(employee.FirstName);
+    }
+}
