c0ef6d5fcd7f0df067b2bd60d180109a4f116b0b
aya fayad <ml.aya.fayad@gmail.com>
2025-05-05 19:29:44 +0300

    abstract

--- inherited/Program.cs
+++ inherited/Program.cs
@@ -1,8 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-public class BaseEmployee
+
+
+public abstract class BaseEmployee
 {
+
+    protected Guid Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+
+    public BaseEmployee()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public abstract decimal CalculatePay(); //Ab
 
     public virtual string GetEmployeeDetail()
     {
@@ -10,24 +21,61 @@ public class BaseEmployee
     }
 }
 
+
+
 public class Houremployee : BaseEmployee
 {
     public decimal HourlRate { get; set; }
+    public int HoursWorked { get; set; }
 
-    public decimal calculateWeeklyPay(int hourWorked)
+    public override decimal CalculatePay()
     {
-        return HourlRate * hourWorked;
+        return HourlRate * HoursWorked;
     }
+    public string GetIdFromChild()
+    {
+        return $"Employee ID (from child): {Id}"; // ✅ This works!
+    }
+    // public sealed override string GetEmployeeDetail()
+    // {
+    //     return $"Name :{FirstName} {LastName}, Hourly Rate :{HourlRate}";
+    // }
+
+    // public decimal calculateWeeklyPay(int hourWorked)
+    // {
+    //     return HourlRate * hourWorked;
+    // }
 }
 
+
+public class SalariedEmployee : BaseEmployee
+{
+    public decimal Salary { get; set; }
+    public override decimal CalculatePay()
+    {
+        return Salary / 52;
+    }
+}
 public class Program
 {
     public static void Main()
     {
-        BaseEmployee employee = new BaseEmployee();
-        employee.FirstName = "aya";
-        employee.LastName = "fayad";
-        employee.GetEmployeeDetail();
-        Console.WriteLine(employee.FirstName);
+        // BaseEmployee employee = new BaseEmployee(); YOU CANT CREATE NEW OBJ 
+        // employee.FirstName = "aya";
+        // employee.LastName = "fayad";
+
+        // Console.WriteLine(employee.GetEmployeeDetail());
+        Houremployee hourly = new Houremployee();
+        hourly.FirstName = "hope";
+        hourly.LastName = "fayyad";
+        hourly.HourlRate = 10m;
+        hourly.HoursWorked = 40;
+        Console.WriteLine(hourly.GetEmployeeDetail());
+        Console.WriteLine("weekly " + hourly.CalculatePay());
+        SalariedEmployee weekly = new SalariedEmployee();
+        weekly.Salary = 52000;
+        Console.WriteLine(weekly.CalculatePay());
+        Console.WriteLine(hourly.GetIdFromChild());
+
     }
 }
