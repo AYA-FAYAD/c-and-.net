d2daaa01bff524b931a79bd0b722f17fac0722db
aya fayad <ml.aya.fayad@gmail.com>
2025-05-06 17:17:56 +0300

    interface

--- inherited/Program.cs
+++ inherited/Program.cs
@@ -1,19 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-public enum EmployeePymentType
+// public enum EmployeePymentType
+// {
+//     Hourly,
+//     Salary
+// }
+
+public interface IPayable
 {
-    Hourly,
-    Salary
+    decimal CalculatePay();
 }
+
+
+
 public class BaseEmployee
 {
-    public EmployeePymentType PymentType { get; set; }
+    // public EmployeePymentType PymentType { get; set; }
     protected Guid Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public decimal HourlRate { get; set; }
-    public int HoursWorked { get; set; }
-    public decimal Salary { get; set; }
+    // public decimal HourlRate { get; set; }
+    // public int HoursWorked { get; set; }
+    // public decimal Salary { get; set; }
 
 
 
@@ -32,7 +40,37 @@ public class BaseEmployee
     public Guid GetId() => Id;
 }
 
+public class Houremployee : BaseEmployee, IPayable
+{
+    public decimal HourlyRate { get; set; }
+    public int HoursWorked { get; set; }
 
+    public decimal CalculatePay()
+    {
+        return HourlyRate * HoursWorked;
+    }
+
+    public override string GetEmployeeDetail()
+    {
+        return $"Hourly Employee : {FirstName} {LastName} Rate: {CalculatePay()}";
+    }
+
+}
+
+public class SalariedEmployee : BaseEmployee, IPayable
+{
+    public decimal AnnualSalary { get; set; }
+
+    public decimal CalculatePay()
+    {
+        return AnnualSalary / 52;
+    }
+
+    public override string GetEmployeeDetail()
+    {
+        return $"Salaried Employee : {FirstName} {LastName} , per week {CalculatePay()}";
+    }
+}
 
 // public class Houremployee : BaseEmployee
 // {
@@ -69,62 +107,92 @@ public class BaseEmployee
 //     }
 // }
 
-public class EmployeePayCalculator
-{
-    public decimal CalculatePay(BaseEmployee employee)
-    {
-        if (employee.PymentType == EmployeePymentType.Hourly)
-        {
-            return employee.HourlRate * employee.HoursWorked;
-        }
-        else if (employee.PymentType == EmployeePymentType.Salary)
-        {
-            return employee.Salary / 52;
-        }
-        else
-        {
-            throw new Exception("Unkown paymet type");
-        }
+// public class EmployeePayCalculator
+// {
+//     public decimal CalculatePay(BaseEmployee employee)
+//     {
+//         if (employee.PymentType == EmployeePymentType.Hourly)
+//         {
+//             return employee.HourlRate * employee.HoursWorked;
+//         }
+//         else if (employee.PymentType == EmployeePymentType.Salary)
+//         {
+//             return employee.Salary / 52;
+//         }
+//         else
+//         {
+//             throw new Exception("Unkown paymet type");
+//         }
 
 
 
 
-    }
-}
+//     }
+// }
 
 public class Program
 {
     public static void Main()
+
     {
-        // BaseEmployee employee = new BaseEmployee(); YOU CANT CREATE NEW OBJ 
-        // employee.FirstName = "aya";
-        // employee.LastName = "fayad";
-        EmployeePayCalculator calculator = new EmployeePayCalculator();
-        BaseEmployee hourly = new BaseEmployee
-        {
-            FirstName = "aya",
-            LastName = "fayyad",
-            PymentType = EmployeePymentType.Hourly,
-            HourlRate = 10m,
-            HoursWorked = 40
-        };
 
+        BaseEmployee employee = new BaseEmployee();
+        employee.FirstName = "aya";
+        employee.LastName = "fayyad";
 
-        BaseEmployee salaried = new BaseEmployee
+        Houremployee hourly = new Houremployee
         {
             FirstName = "hope",
             LastName = "fayad",
-            PymentType = EmployeePymentType.Salary,
-            Salary = 54000
+            HourlyRate = 10m,
+            HoursWorked = 40
         };
+
+        SalariedEmployee salaried = new SalariedEmployee
+        {
+            FirstName = "Aya",
+            LastName = "Fayad",
+            AnnualSalary = 5400,
+
+        };
+
         Console.WriteLine(hourly.GetEmployeeDetail());
-        Console.WriteLine("Salaried weekly Pay:" + calculator.CalculatePay(hourly));
-        Console.WriteLine("Hourly Employye ID : " + hourly.GetId());
-
-
         Console.WriteLine(salaried.GetEmployeeDetail());
-        Console.WriteLine("Salaried Weekly Pay: " + calculator.CalculatePay(salaried));
-        Console.WriteLine("Salaried Employee ID: " + salaried.GetId());
+
+
+
+
+
+
+        // BaseEmployee employee = new BaseEmployee(); YOU CANT CREATE NEW OBJ 
+        // employee.FirstName = "aya";
+        // employee.LastName = "fayad";
+        // EmployeePayCalculator calculator = new EmployeePayCalculator();
+        // BaseEmployee hourly = new BaseEmployee
+        // {
+        //     FirstName = "aya",
+        //     LastName = "fayyad",
+        //     PymentType = EmployeePymentType.Hourly,
+        //     HourlRate = 10m,
+        //     HoursWorked = 40
+        // };
+
+
+        // BaseEmployee salaried = new BaseEmployee
+        // {
+        //     FirstName = "hope",
+        //     LastName = "fayad",
+        //     PymentType = EmployeePymentType.Salary,
+        //     Salary = 54000
+        // };
+        // Console.WriteLine(hourly.GetEmployeeDetail());
+        // Console.WriteLine("Salaried weekly Pay:" + calculator.CalculatePay(hourly));
+        // Console.WriteLine("Hourly Employye ID : " + hourly.GetId());
+
+
+        // Console.WriteLine(salaried.GetEmployeeDetail());
+        // Console.WriteLine("Salaried Weekly Pay: " + calculator.CalculatePay(salaried));
+        // Console.WriteLine("Salaried Employee ID: " + salaried.GetId());
 
         // Console.WriteLine(employee.GetEmployeeDetail());
         // Houremployee hourly = new Houremployee();
